2acc28c66c7e08ff7b9779ec0c0627c879e71a47
Olit <93828847+OlitHub@users.noreply.github.com>
2023-10-20 13:35:27 +0200

    fix: tests

--- tests/test_rcs_fun.py
+++ tests/test_rcs_fun.py
@@ -7,7 +7,7 @@ from rcs_project import rcs_fun as fun
 def test_get_color():
     """Test the get_color function
     """
-    img = Image.open("../data/test_image.png")
+    img = Image.open("data/test_image.png")
     
     r1 = np.random.randint(0, img.size[0])
     r2 = np.random.randint(0, img.size[1])
@@ -17,7 +17,7 @@ def test_get_color():
 def test_get_color2():
     """Test the get_color2 function
     """
-    img = Image.open("../data/test_image.png")
+    img = Image.open("data/test_image.png")
     
     r1 = np.random.randint(0, img.size[0])
     r2 = np.random.randint(0, img.size[1])

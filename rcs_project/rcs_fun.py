e4b976a5291498607ae642fb2b9b19be5906f874
FabioGadegbeku <fabio.gadegbeku.etu@univ-lille.fr>
2023-10-20 12:14:09 +0200

    fix

--- rcs_project/rcs_fun.py
+++ rcs_project/rcs_fun.py
@@ -5,8 +5,6 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 im = Image.open('./data/wallpaper.jpg')
-print(im.format, im.size, im.mode)
-
 def hex_pix2(im,center,hex_size):
     x=center[0]
     y=center[1]
@@ -16,15 +14,15 @@ def hex_pix2(im,center,hex_size):
     max_y = int(np.floor(y + hex_size))
     pixels = np.zeros((max_x-min_x,max_y-min_y,3))
     # Iterate through the hexagon
-    i=-1
+    i=0
     j=0
     for row in range(min_x,max_x):
-        i+1
         for col in range(min_y,max_y):
             pixels[i][j] = im.getpixel((row,col))
             j+1
+        i+1
     pixel = np.mean(pixels, axis=(0,1))
 
-    return(pixel)
+    return(pixels)
 
-print(hex_pix2(im,(50,50),2))
+print(hex_pix2(im,(50,100),10))

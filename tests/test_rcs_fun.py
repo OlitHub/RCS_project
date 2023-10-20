838c60bcc63b7d561e42bbca275ea5308aafca5c
Olit <93828847+OlitHub@users.noreply.github.com>
2023-10-20 09:01:47 +0200

    feat: added pytest

--- tests/test_rcs_fun.py
+++ tests/test_rcs_fun.py
@@ -1,7 +1,2 @@
-"""test_rcs_fun.py - test the rcs_fun module
+"""test file for rcs_project/rcs_fun.py
 """
-
-from rcs_project import __version__
-
-def test_version():
-    assert __version__ == "0.1.0"

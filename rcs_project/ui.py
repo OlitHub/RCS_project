358db2c34869d662a1834929f1876cd501dffb95
FabioGadegbeku <fabio.gadegbeku.etu@univ-lille.fr>
2023-10-21 15:36:38 +0200

    feat: added reset save and exit button to UI

--- rcs_project/ui.py
+++ rcs_project/ui.py
@@ -6,9 +6,11 @@ import numpy as np
 from PIL import Image, ImageDraw, ImageTk
 import rcs_ui_fun as fun
 import svgwrite as svg
+current_image = None
 
 def open_image():
-    global panel # make panel a global variable so we can access it later
+    """Open a file dialog window and open the selected image"""
+    global panel
     file_path = filedialog.askopenfilename()
     input_image = Image.open(file_path)
     integer_parameter = int(integer_entry.get())
@@ -16,11 +18,20 @@ def open_image():
     panel = tk.Label(window, image=processed_image)
     panel.image = processed_image # keep a reference
     panel.pack()
+    
 
-max_width = 700
+    # Disable the open button
+    open_button.config(state="disabled")
+    message_label.config(text="Image displayed. Click 'Reset' to pick another image or 'Save Image' to save.")
+
+# Function to clear the image
+
+max_width = 650
 max_height = 800
 
 def scale_image(image):
+    global current_image
+    """Scale down the image if it is too large to display"""
     width, height = image.size
     if width > max_width or height > max_height:
         # If the image is larger, scale it down
@@ -30,15 +41,32 @@ def scale_image(image):
 
         # Resize the image
         image = image.resize((new_width, new_height), Image.LANCZOS)
+    current_image = image
     image = ImageTk.PhotoImage(image)
     return image
 
+def save_image():
+    """Save the current image"""
+    if current_image:
+        save_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
+        if save_path:
+            current_image.save(save_path)
+
+
 def reset_image():
+    """Reset the image to none and reactivate the open button"""
     panel.pack_forget()
+    open_button.config(state="normal")
+    message_label.config(text="No image displayed. Click 'Open Image'.")
+
+
 
 window = tk.Tk()
 window.title("Hexagonal Grid Image Processing")
 window.geometry("800x800")
+
+message_label = tk.Label(window, text="No image displayed. Click 'Open Image'.")
+message_label.pack()
 
 open_button = tk.Button(window, text="Open Image", command=open_image)
 open_button.pack()
@@ -48,8 +76,19 @@ integer_entry = tk.Entry(window)
 integer_entry.insert(0, "10")  # Set a default value
 integer_entry.pack()
 
-reset_button = tk.Button(window, text="Reset", command=reset_image)
-reset_button.pack()
+button_frame = tk.Frame(window)
+button_frame.pack(pady=10)
 
+# Create the Reset button
+reset_button = tk.Button(button_frame, text="Reset",command=reset_image)
+reset_button.grid(row=0, column=0, padx=5)
+
+# Create the Save button
+save_button = tk.Button(button_frame, text="Save",command=save_image)
+save_button.grid(row=0, column=1, padx=5)
+
+# Create the Exit button
+exit_button = tk.Button(button_frame, text="Exit",command=window.quit)
+exit_button.grid(row=0, column=2, padx=5)
 
 window.mainloop()
